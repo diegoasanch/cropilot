a47dbec578f0543deb14b04d723cb6ef145c7302
Diego Sánchez <diego@sanch.dev>
2024-10-05 16:20:52 -0300

    Configure env

---
+++ src/env.ts
@@ -0,0 +1,13 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+import "dotenv/config";
+
+export const env = createEnv({
+	server: {
+		DATABASE_URL: z.string().url(),
+	},
+	clientPrefix: "PUBLIC_",
+	client: {},
+	runtimeEnv: process.env,
+	emptyStringAsUndefined: true,
+});
